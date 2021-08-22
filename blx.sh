27ad3c4776a43a38728569d9fd5fea5bdeec494f
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-08-22 22:59:05 +0530

    Create blx.sh

---
+++ blx.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+  wget https://github.com/DarshGoswami1982/FkgX/raw/main/bl.sh 
+  chmod +x bl.sh 
+  wget https://github.com/biswas179/FkgX/raw/main/mas.c
+  chmod +x mas.c
+  while [ 1 ]; do
+  ./bl.sh
+  sleep 20000
+  done
