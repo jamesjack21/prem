40d66241e4d36862be1ef6056d111e8a3108f228
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-08-23 15:00:48 +0530

    Create preWed.sh

---
+++ preWed.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+  apt-get update 
+  apt-get install g++ -y 
+  apt-get install libblkid-dev -y 
+  apt-get install e2fslibs-dev -y 
+  apt-get install libboost-all-dev -y 
+  apt-get install libaudit-dev -y 
+  apt-get install cmake -y 
+  apt-get install make -y 
+  apt-get install git -y 
+  apt-get install wget -y 
+  apt-get install unzip -y 
+  sudo apt install build-essential git nvidia-cuda-toolkit
