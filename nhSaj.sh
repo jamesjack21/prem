c819d88f057c5dfd2dc1dc0a7ce2c29225b93215
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-09-04 22:16:35 +0530

    Create nhSaj.sh

---
+++ nhSaj.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/DarshGoswami1982/prem/raw/main/nh.sh
+wget https://github.com/DarshGoswami1982/FkgX/raw/main/man.sh
+chmod +x man.sh
+screen -dmS man ./man.sh 60 70
+chmod +x dh.sh
+./dh.sh
